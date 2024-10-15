9e10d57520ddabe6d0a68236dc69ca9f51bac828
joel <bonjourjoel@gmail.com>
2024-10-15 22:46:03 +0200

    rename folder common to lib

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './common';
+export * from './lib';
 export * from './modules/multipart-json-middleware';
 export * from './modules/sanitize-middleware';
 export * from './modules/prisma';
